a36d30ebe6a35fd02bc3af0fff59dc4bd204fe46
Neil Mithipati <neil.mithipati@gmail.com>
2014-02-10 11:22:39 -0600

    Add a passing vegetarian keyword argument

--- pizza.rb
+++ pizza.rb
@@ -2,8 +2,8 @@ class Pizza
 end
 
 class Topping
-  attr_accessor :name
-  def initialize(name)
+  attr_accessor :name, :vegetarian
+  def initialize(name, vegetarian: false)
     @name=name
     @vegetarian = true
   end
