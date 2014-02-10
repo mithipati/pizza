bffe508f481b06579884ce00e08eb4b2e7398972
Neil Mithipati <neil.mithipati@gmail.com>
2014-02-10 11:30:58 -0600

    Add failing Pizza .initialize method

--- pizza.rb
+++ pizza.rb
@@ -4,7 +4,7 @@ end
 class Topping
   attr_accessor :name, :vegetarian
   def initialize(name, vegetarian: false)
-    @name=name
-    @vegetarian = true
+    @name = name
+    @vegetarian = vegetarian
   end
 end
